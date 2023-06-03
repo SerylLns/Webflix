f9872e0d6d5f770a899782d0cbdb4da4fe85d5b1
seryl lounis <seryllounis@gmail.com>
2023-06-03 15:50:04 +0200

    Final

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>WebFlix</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <link rel="shortcut icon" href="icone.png" type="images/png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
