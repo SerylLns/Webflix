74f27a5d230adafdbe6db8129ab98d74c283cbf2
seryl lounis <seryllounis@gmail.com>
2023-06-03 12:49:48 +0200

    Feat: Favorite feature

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -34,14 +34,23 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
     mutateFavorites();
   }, [movieId, isFavorite, currentUser, mutate, mutateFavorites]);
 
-  const Icon = isFavorite ? AiOutlineCheckCircle : AiOutlinePlus;
+  const Icon = isFavorite ? AiOutlineCheck : AiOutlinePlus;
 
   return (
     <div
       onClick={toggleFavorites}
-      className="cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex items-center justify-center transition hover:border-neutral-300 hover:bg-white group/button hover:opacity-90"
+      className={`${
+        isFavorite ? "bg-white" : "hover:bg-white"
+      } cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 p-0.5 border-white border-2 rounded-full flex items-center justify-center transition hover:border-neutral-300 group/button hover:opacity-90`}
     >
-      <Icon className="text-white group-hover/button:text-black" size={25} />
+      <Icon
+        className={`${
+          isFavorite
+            ? " group-hover/button:text-black"
+            : "text-white group-hover/button:text-black"
+        }`}
+        size={22}
+      />
     </div>
   );
 };
